0cecc9af02fb1e479f944ca489325c268c887078
leopoldh <leopold.hermansson@gmail.com>
2022-03-08 21:54:53 +0100

    added caching

--- src/parse.py
+++ src/parse.py
@@ -1,18 +1,17 @@
-from functools import lru_cache
+from functools import lru_cache, partial
 from typing import List, Optional
 import requests
 import string
+import requests_cache
 from multiprocessing.pool import ThreadPool
 
 valid_letters = set(i for i in string.ascii_letters + string.digits+"-+_?^$@!*")
 
-@lru_cache(100)
-def cached_get(url):
-    return requests.get(url)
+requests_cache.install_cache(cache_name='request_cache', backend='sqlite', expire_after=180)
 
 def parse_content(links: set, url:str):
     try:
-        content = cached_get(url).content
+        content = requests.get(url).content
         if b"twitch.tv/" in content:
             for link in content.split(b"twitch.tv/")[1:]:
                 s = ""
@@ -26,20 +25,20 @@ def parse_content(links: set, url:str):
 
 class Faceit:
     def get_steam_id(faceit_nickname: str) -> Optional[str]:
-        r = cached_get(f"https://api.faceit.com/users/v1/nicknames/{faceit_nickname}")
+        r = requests.get(f"https://api.faceit.com/users/v1/nicknames/{faceit_nickname}")
         try:
             return r.json()["payload"]["platforms"]["steam"]["id64"]
         except:
             return None
 
-    def parse_player(links: set, faceit_data: str):
+    def parse_player(faceit_data: str,links: set):
         faceit_nickname = faceit_data["nickname"]
         steam_id = Faceit.get_steam_id(faceit_nickname)
         if steam_id:
             parse_content(links, f"http://steamcommunity.com/profiles/{steam_id}")
 
     def get_players_data(game_id: str) -> List:
-        data = cached_get(f"https://api.faceit.com/match/v2/match/{game_id}").json()
+        data = requests.get(f"https://api.faceit.com/match/v2/match/{game_id}").json()
         return data["payload"]["teams"]["faction1"]["roster"]+data["payload"]["teams"]["faction2"]["roster"]
 
     def parse_game(game_id) -> List:
@@ -47,7 +46,7 @@ class Faceit:
         parse_content(links, f"https://api.faceit.com/match/v2/match/{game_id}")
         players_data = Faceit.get_players_data(game_id)
         with ThreadPool(10) as p:
-            p.map(Faceit.parse_player, links*len(players_data), players_data)
+            p.map(partial(Faceit.parse_player,links=links), players_data)
         return list(links)
 
     
